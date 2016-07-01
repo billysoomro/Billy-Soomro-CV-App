f34c9c679314a602e5650c89e013d4dc59ef7807
billabong02 <bilawal_soomro2004@yahoo.com>
2016-07-01 13:09:53 +0100

    Added new material icons and some logos.

--- app/src/main/java/com/example/bilawal/billysoomro/MainActivity.java
+++ app/src/main/java/com/example/bilawal/billysoomro/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.personal_profile) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.experience) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.education) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.skill_set) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.interests) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.contact) {
 
         }
 
